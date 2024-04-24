3d0048b34c238781ddf812b9544066826348c2b8
hitonanode <32937551+hitonanode@users.noreply.github.com>
2024-04-25 00:18:52 +0900

    add unit test for maximization

---
+++ test/test_maximize.py
@@ -0,0 +1,30 @@
+import pytest
+
+from modeler import LPModel, LPSense, LPStatus, LPVar
+
+
+def test_maximize() -> None:
+    x = LPVar("x", lower_bound=1, upper_bound=2)
+
+    model = LPModel(sense=LPSense.MAXIMIZE)
+
+    model.set_objective(x + 1.0)
+
+    model.solve()
+
+    assert model.status == LPStatus.OPTIMAL
+
+    assert x.value() == pytest.approx(2.0)
+    assert model.objective.value() == pytest.approx(3.0)
+
+
+def test_maximize_const_no_variable() -> None:
+    model = LPModel(sense=LPSense.MAXIMIZE)
+
+    model.set_objective(-2.0)
+
+    model.solve()
+
+    assert model.status == LPStatus.OPTIMAL
+
+    assert model.objective.value() == pytest.approx(-2.0)
